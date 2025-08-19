99f2825af9c4cbc7bc05dc0341951ad55b19cdc5
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-19 13:41:19 +0530

    feat: add fade overlay image to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,13 @@ const Hero = () => {
             <div className=" w-[1380px] h-[617px] rounded-[15px]" style={{
                 background: 'linear-gradient(to right, #C1DEE8, #FBD9B9)'
             }}>
+                <img
+                    src="/fade.png"
+                    alt="fade overlay"
+                    className=" w-full h-full object-cover rounded-[15px] opacity-50"
+                />
+
+
 
             </div>
         </div>
