50e8a2b71091c2f0ee5d7ef55728226d0a5e25e7
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-20 00:19:43 +0530

    feat: update global styles and enhance NavBar layout with new links

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
                 <div className="flex items-center pl-[40px]">
                     <Image src="/Logo.svg" alt="Logo" width={94} height={28}/>
                 </div>
-                <div className="flex-1 flex items-center pl-[376px]">
+                <div className="flex-1 flex items-center justify-center pl-[242px]">
                     <div className="flex justify-center items-center w-[571px]  h-[60px] bg-[#FEFEFF] rounded-[40px]">
                         <div className="flex flex-row  gap-[35px]">
                             <Link href="/">
@@ -36,8 +36,24 @@ const NavBar = () => {
                                     Listings
                                 </span>
                             </Link>
-                            <Image src="/search.svg" alt="search" width={24} height={24}/>
+                            <Link href="/">
+                                <Image src="/search.svg" alt="search" width={24} height={24}/>
+                            </Link>
                         </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center pr-[24px] gap-[30px]">
+                    <Link href="/">
+                        <div className="font-[400] text-[16px] leading-[120%] ">
+                            Other services
+                        </div>
+                    </Link>
+                    <div className="w-[158px] h-[60px] bg-white border border-[#BEBEBE] rounded-[40px] flex items-center justify-center ">
+                        <Link href="/">
+                            <div className="font-[400] text-[16px] leading-[120%] whitespace-nowrap">
+                                Contact us
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </nav>
