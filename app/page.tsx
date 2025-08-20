4dc4d29967bc03c084771558b7ff3ca612791388
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-20 07:14:15 +0530

    feat: add Steps component to Home page for enhanced user guidance

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
+import Steps from "@/components/Steps";
 
 export default function Home() {
     return (
         <main className="px-[30px] pt-[34px] pb-[110px]">
             <NavBar/>
             <Hero/>
+            <Steps/>
         </main>
     );
 }
---
+++ components/Steps.tsx
@@ -0,0 +1,18 @@
+const Steps = () => {
+    return(
+        <div className="relative flex flex-col items-center justify-center ">
+            <div className="relative flex  items-center mt-[108px]">
+                <h2 className="text-[#000000] font-[400] text-[16px] leading-[120%]">
+                    Three steps. Three minutes.
+                </h2>
+            </div>
+            <div className="relative flex  items-center mt-[16px]">
+                <h1 className="text-[#000000] font-[600] text-[50px] leading-[120%]">
+                    Everything should be this easy.
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Steps;
