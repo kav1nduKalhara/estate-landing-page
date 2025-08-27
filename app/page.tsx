805ee34fe2c383a7369ae5e25a83a3cbd38155cb
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-27 09:40:14 +0530

    refactor: update layout of Home component for improved spacing and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import Steps from "@/components/Steps";
 import Guide from "@/components/Guide";
 import RecentProject from "@/components/RecentProject";
 import RecentProjectCard from "@/components/RecentProjectCard";
-import {Globe} from "lucide-react";
 import Global from "@/components/Global";
 import Transfer from "@/components/Transfer";
 import Subscribe from "@/components/Subscribe";
 
+
 export default function Home() {
     return (
-        <main className="px-4 sm:px-6 md:px-12 lg:px-20 xl:px-40 pt-6 pb-20 max-w-[1440px] mx-auto">
+        <main className="px-[30px] pt-[34px] pb-[110px]">
             <NavBar/>
             <Hero/>
             <Steps/>
